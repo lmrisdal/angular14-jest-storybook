818e6b682f8521c43b1cbf78502938e7f141cb53
Lars Risdal <larsrisdal@gmail.com>
2022-05-22 12:50:11 +0200

    added eslint+prettier linting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,11 @@
 // import { AppComponent } from "./app.component"
 
-import { AppComponent } from './app.component';
-import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component'
+import { TestBed } from '@angular/core/testing'
 
-const appComponentFixture = TestBed.createComponent(AppComponent);
-const appComponent = appComponentFixture.debugElement.componentInstance;
+const appComponentFixture = TestBed.createComponent(AppComponent)
+const appComponent = appComponentFixture.debugElement.componentInstance
 
 test(`the title is 'angular14-jest-storybook`, () => {
-  expect(appComponent.title).toEqual('angular14-jest-storybook');
-});
+  expect(appComponent.title).toEqual('angular14-jest-storybook')
+})
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular14-jest-storybook';
+  title = 'angular14-jest-storybook'
+  asdasd = 1
+
+  constructor() {}
 }
